c79a74c912bc12be73056ca39946d2363021e307
ajay <ajaykrjha93@gmail.com>
2019-02-11 14:48:04 +0530

    changed action creator to be return object having mqtt as key having function as value having client as arguments rather than to returning the direct fuction having mqtt client as arguments

---
+++ redux/middleware/index.js
@@ -0,0 +1,14 @@
+const  mqttMiddleware = (client) => {
+    return ({ dispatch, getState }) => {
+      return next => (action) => {
+        if (typeof action === 'function') {
+          return action(dispatch, getState);
+        }else if( typeof action === 'object' && action.mqtt !== undefined ){
+            return action.mqtt(client, dispatch, getState)
+        }
+        return next(action);
+      };
+    };
+}
+
+export default mqtt;
